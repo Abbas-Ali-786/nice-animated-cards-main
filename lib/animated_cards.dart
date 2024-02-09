00d732157e1dbbcd79ea1df6f828e9491129d6d2
Abbas Ali <wwwabbasmughal786@gmail.com>
2024-02-09 20:20:24 +0500

    Update animated_cards.dart

--- lib/animated_cards.dart
+++ lib/animated_cards.dart
@@ -14,6 +14,7 @@ class _AnimatedCardsState extends State<AnimatedCards> {
   bool isHovered4 = false;
   @override
 
+
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black87,
@@ -33,6 +34,7 @@ class _AnimatedCardsState extends State<AnimatedCards> {
                   isHovered1 = false;
                 });
               },
+
               child: Stack(
                 children: [
                   AnimatedContainer(
